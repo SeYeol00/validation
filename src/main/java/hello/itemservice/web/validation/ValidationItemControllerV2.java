94b81828687b8862542c889cbb81cf8af4319bc9
WE-DA-pluggg <79959576+SeYeol00@users.noreply.github.com>
2022-10-12 17:48:51 +0900

    10/12

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -45,7 +45,7 @@ public class ValidationItemControllerV2 {
         return "validation/v2/addForm";
     }
 
-    @PostMapping("/add")
+    //@PostMapping("/add")//                          이게 핵심, 이걸로 에러 객체 처리
     public String addItemV1(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
         //검증 로직
         if(!StringUtils.hasText(item.getItemName())){
@@ -63,6 +63,82 @@ public class ValidationItemControllerV2 {
             int resultPrice = item.getQuantity() * item.getPrice();
             if(resultPrice<10000){// 이건 새로운 에러를 지정할 때, 즉 필드 값이 없을 때
                 bindingResult.addError(new ObjectError("item","가격 * 수량의 합은 10,000원 이상이어야합니다. 현재 값: "+resultPrice));
+
+            }
+        }
+        // 검증에 실패하면 다시 입력 폼으로
+        // bindingResult의 작동 방식이 중요하다.
+        if(bindingResult.hasErrors()){
+            log.info("errors={}",bindingResult);
+            // 바인딩 리절트는 자동으로 모델에 넘어가므로 안 담아도 된다.
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    //@PostMapping("/add")//                          이게 핵심, 이걸로 에러 객체 처리
+    public String addItemV2(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        //검증 로직
+        if(!StringUtils.hasText(item.getItemName())){
+            //                                  모델 어트리뷰트에 담기는 이름         rejectedValue 넣으면 값 유지
+            bindingResult.addError(new FieldError("item","itemName",item.getItemName(),false,null,null,"상품 이름은 필수입니다."));
+
+        }
+        if(item.getPrice()==null||item.getPrice()<1000|| item.getPrice()>1000000){
+            bindingResult.addError(new FieldError("item","price",item.getPrice(),false,null,null,"가격은 천원에서 백만원까지 허용합니다."));
+
+        }
+        if(item.getQuantity()==null||item.getQuantity()>=9999){
+            bindingResult.addError(new FieldError("item","quantity",item.getQuantity(),false,null,null,"수량은 최대 9,999까지 허용합니다."));
+        }
+        // 특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice()!=null && item.getQuantity()!= null){
+            int resultPrice = item.getQuantity() * item.getPrice();
+            if(resultPrice<10000){// 이건 새로운 에러를 지정할 때, 즉 필드 값이 없을 때
+                bindingResult.addError(new ObjectError("item",null,null,"가격 * 수량의 합은 10,000원 이상이어야합니다. 현재 값: "+resultPrice));
+
+            }
+        }
+        // 검증에 실패하면 다시 입력 폼으로
+        // bindingResult의 작동 방식이 중요하다.
+        if(bindingResult.hasErrors()){
+            log.info("errors={}",bindingResult);
+            // 바인딩 리절트는 자동으로 모델에 넘어가므로 안 담아도 된다.
+            return "validation/v2/addForm";
+        }
+
+        // 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")//                          이게 핵심, 이걸로 에러 객체 처리
+    public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+        //검증 로직
+        if(!StringUtils.hasText(item.getItemName())){// 에러 메세지를 코드화 시킨 것
+            //                                  모델 어트리뷰트에 담기는 이름         rejectedValue 넣으면 값 유지             여기서 메세지를 에러 프로퍼티즈에서 가져와서 디폴트 메세지 필요없음
+            bindingResult.addError(new FieldError("item","itemName",item.getItemName(),false,new String[]{"required.item.itemName"},null,null));
+
+        }
+        if(item.getPrice()==null||item.getPrice()<1000|| item.getPrice()>1000000){//                                                                 조건 파라미터, 에러 프로퍼티에서 파라미터가 두 개라
+            bindingResult.addError(new FieldError("item","price",item.getPrice(),false,new String[]{"range.item.price"},new Object[]{1000.1000000},null));
+
+        }
+        if(item.getQuantity()==null||item.getQuantity()>=9999){//                                                                                       error.properties에서 요구하는 파라미터
+            bindingResult.addError(new FieldError("item","quantity",item.getQuantity(),false,new String[]{"max.item.quantity"},new Object[]{9999},null));
+        }
+        // 특정 필드가 아닌 복합 룰 검증
+        if(item.getPrice()!=null && item.getQuantity()!= null){
+            int resultPrice = item.getQuantity() * item.getPrice();
+            if(resultPrice<10000){// 이건 새로운 에러를 지정할 때, 즉 필드 값이 없을 때
+                bindingResult.addError(new ObjectError("item",new String[]{"totalPriceMin"},new Object[]{10000, resultPrice},null));
 
             }
         }
